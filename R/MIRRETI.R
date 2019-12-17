1510bc480d4e4bc2fd5be07259f4f7f97cef6fd9
Evelyn Scheibling <evelyn@norm.exbio.wzw.tum.de>
2019-12-18 00:43:14 +0100

    MIRRETI 3.1.2 Simplification and completion of data preprocess workflow for SPONGE interaction filter

--- R/MIRRETI.R
+++ R/MIRRETI.R
@@ -2,14 +2,7 @@ library(data.table)
 library(graphics)
 library(dplyr)
 library(biomaRt)
-
-
-#                                   Parameter
-#-------------------------------------------------------------------------------------
-
-primary.disease <- "breast invasive carcinoma"
-sample.type.id <- "11"
-out.filepath <- "/nfs/home/students/evelyn/bachelor/data/thirdShot/spongerun_data_breastinvasivecarcinome_solidtissuenormal.RDS"
+library(SPONGE)
 
 
 
@@ -17,29 +10,43 @@ out.filepath <- "/nfs/home/students/evelyn/bachelor/data/thirdShot/spongerun_dat
 #                                 Basic read methods
 #-------------------------------------------------------------------------------------
 
-read.expression.file <- function(expression_filepath){
-  expr_data <- data.frame(fread(expression_filepath, header = T, sep = "\t"), row.names = 1, check.names = F)
+
+read.expression.file <- function(expression.filepath){
+  expr_data <- data.frame(fread(expression.filepath, header = T, sep = "\t"), row.names = 1, check.names = F)
   rownames(expr_data) <- lapply(strsplit(rownames(expr_data), "\\."), '[', 1)
   return(expr_data)
 }
 
-read.interaction.file <- function(interaction_filepath){
-  interactions <- fread(interaction_filepath, header = T, sep = "\t")
+read.interaction.file <- function(interaction.filepath){
+  interactions <- fread(interaction.filepath, header = T, sep = "\t")
   return(interactions)
 }
 
-read.sample.annotation.file <- function(sampleannotation_filepath){
-  sample_annotation <- read.csv(sampleannotation_filepath, header = T, sep = "\t")
+read.interaction.files <- function(utr5.filepath, cds.filepath, utr3.filepath){
+  utr5 <- read.interaction.file(utr5.filepath)
+  utr5$genomic_region <- rep("5UTR", nrow(utr5))
+  cds <- read.interaction.file(cds.filepath)
+  cds$genomic_region <- rep("CDS", nrow(cds))
+  utr3 <- read.interaction.file(utr3.filepath)
+  utr3$genomic_region <- rep("3UTR", nrow(utr3))
+  interactions <- rbind(utr5, cds, utr3)
+  return(interactions)
+}
+
+read.sample.annotation.file <- function(sampleannotation.filepath){
+  sample_annotation <- read.csv(sampleannotation.filepath, header = T, sep = "\t")
   return(sample_annotation)
 }
+
 
 
 #-------------------------------------------------------------------------------------
 #                               Preprocess methods
 #-------------------------------------------------------------------------------------
 
-# Finished but needs testing
-preproc.idconversion.refseqtoensembl(interactions, id.type = 'both'){
+
+# Finished 
+preproc.idconversion.refseqtoensembl <- function(interactions, id.type = 'both'){
   
   # Step 1: Get RefSeq IDs from interactions data
     refseq_ids <- unique(interactions$mRNA)
@@ -71,8 +78,12 @@ preproc.idconversion.refseqtoensembl(interactions, id.type = 'both'){
       colnames(predrefseq_ensembl) <- c('mRNA', 'ensembl_transcript_id', 'Genesymbol')
       mappings <- rbind(refseq_ensembl, predrefseq_ensembl)
     } else {
-      cat("\nIncorrect value for parameter id_type\nPlease choose between 'validated', 'predicted' and 'both'\n")
-      quit
+      cat("\nIncorrect value for parameter id_type\nPlease choose between 'validated', 'predicted' and 'both'\nRefSeq to Ensemble IDs conversion stopped...\n")
+      return(NULL)
+    }
+    if(is.null(mappings)){
+      cat("\nNo Ensemble IDs found\nRefSeq to Ensemble IDs conversion stopped...\n")
+      return(NULL)
     }
   
   # Step 3: Remove entries where no Ensemble IDs were found
@@ -81,6 +92,7 @@ preproc.idconversion.refseqtoensembl(interactions, id.type = 'both'){
   # Step 4: Merge ID mapping to interactions data
     interactions <- interactions[interactions$mRNA %in% mappings$mRNA, ]
     interactions <- merge(interactions, mappings, by = c("mRNA", "Genesymbol"), allow.cartesian = T)
+  return(interactions)
 }
 
 # Finished
@@ -115,7 +127,7 @@ preproc.filter.samples <- function(mir_expr, tpm_expr, sample_annotation, primar
       cat("---------------------------------\n\n")
       print(sample_types)
       cat("\n")
-      .Internal(.invokeRestart(list(NULL, NULL), NULL))
+      return(list(NULL, NULL))
     } else if(!valid.disease.type) {
       sample_list <- sample_annotation[sample_annotation$sample_type_id == sample.type.id & 
                                          !is.na(sample_annotation$sample_type_id), ]
@@ -130,15 +142,17 @@ preproc.filter.samples <- function(mir_expr, tpm_expr, sample_annotation, primar
     
     # Report if no samples were found
     if(length(sample_list) < 1){
-      cat("\nRetrieved sample list of size 0.\n")
+      cat("\nRetrieved sample list of size 0.\nFilter expr data for samples stopped...\n")
+      return(list(NULL, NULL))
     }
   
   # Step 2: Subset mir_expr & gene_expr columns for found sample IDs    
     mir_expr <- mir_expr[ , colnames(mir_expr) %in% sample_list]
     tpm_expr <- tpm_expr[ , colnames(tpm_expr) %in% sample_list]
+  return(list(mir_expr, tpm_expr))
 }
 
-#Finished
+# Finished
 # na.equivalent:  miRNA        0
 #                 transcripts  -9.9658
 preproc.filter.na <- function(expr_data, na.equivalent, max.na.percent = 0.2){
@@ -146,6 +160,7 @@ preproc.filter.na <- function(expr_data, na.equivalent, max.na.percent = 0.2){
   expr_data <- expr_data[na_content/ncol(expr_data) <= max.na.percent, ]
   na_content <- colSums(expr_data == na.equivalent)
   expr_data <- expr_data[ , na_content/nrow(expr_data) <= max.na.percent]
+  return(expr_data)
 }
 
 # Finished
@@ -154,6 +169,7 @@ preproc.filter.variance <- function(expr_data, var.threshold = 0.2){
   expr_data <- expr_data[order(expr_data$variance, decreasing = T), ]
   threshold <- floor((1-var.threshold) * nrow(expr_data))
   expr_data <- expr_data[1:threshold, colnames(expr_data) != 'variance']
+  return(expr_data)
 }
 
 # Finished
@@ -161,45 +177,43 @@ preproc.subset.variance <- function(expr_data, top.number = 10){
   expr_data$variance <- apply(data.frame(expr_data), 1, var)
   expr_data <- expr_data[order(expr_data$variance, decreasing = T), ]
   expr_data <- expr_data[1:top.number, colnames(expr_data) != 'variance']
+  return(expr_data)
 }
 
-
+# Finished
 preproc.crossfilter <- function(mir_expr, tpm_expr, interactions){
-  # Step 1: filter for intersecting samples
-  
-  # Step 2: filter for intersecting miRNAs
-  
-  # Step 3: filter for intersectiont transcripts
+  mir_expr <- mir_expr[rownames(mir_expr) %in% interactions$miRNA , colnames(mir_expr) %in% colnames(tpm_expr)]
+  tpm_expr <- tpm_expr[rownames(tpm_expr) %in% interactions$ensembl_transcript_id, colnames(tpm_expr) %in% colnames(mir_expr)]
+  interactions <- interactions[interactions$miRNA %in% rownames(mir_expr) & interactions$ensembl_transcript_id %in% rownames(tpm_expr), ]
+  return(list(mir_expr, tpm_expr, interactions))
+}
+
+# Finished
+preproc.data.spongefilter <- function(mir_expr, tpm_expr, interactions){
+  mir_expr <- t(mir_expr)
+  tpm_expr <- t(tpm_expr)
+  #rownames(expr_data) <- gsub("\\.", "-", rownames(expr_data))
+  interactions_matrix <- as.matrix(table(interactions[ , c("ensembl_transcript_id", "miRNA")]))
+  return(list(mir_expr, tpm_expr, interactions_matrix))
 }
 
 
 
-
-#                                    Workflow
+#-------------------------------------------------------------------------------------
+#                               Preprocess methods
 #-------------------------------------------------------------------------------------
 
-process.data.for.SPONGEinteractionFilter <- function(mir_expr, tpm_expr, interactions, sample_annotation, primary_disease, sample_type_id, out_filepath){
-  #Step 1
-  sample_list <- retrieve.tcgasample.list(sample_annotation, primary_disease = primary_disease, sample_type_id = sample_type_id)
-  mir_expr <- preprocess.exprdata(mir_expr, 
-                                  id_list = unique(interactions$miRNA), 
-                                  sample_list = sample_list)
-  interactions <- interactions[interactions$miRNA %in% colnames(mir_expr),]
-  
-  
-  #Step 2
-  id_mappings <- convert.refseqids.to.ensemblids(unique(interactions$mRNA))
-  id_mappings <- id_mappings[id_mappings$ensembl_transcript_id %in% rownames(tpm_expr),]
-  
-  
-  #Step 3
-  interactions <- merge(interactions, id_mappings, by = c("mRNA", "Genesymbol"), allow.cartesian = T)
-  interactions_matrix <- as.data.frame.matrix(table(select(interactions, "ensembl_transcript_id", "miRNA")))
-  tpm_expr <- preprocess.exprdata(tpm_expr,
-                                  id_list = unique(interactions$ensembl_transcript_id),
-                                  sample_list = sample_list)
-  save(as.matrix(mir_expr), as.matrix(tpm_expr), as.matrix(interactions_matrix), file = out_filepath)
-}
+
+sponge.filter <- function(tpm_expr, mir_expr, interactions_matrix){
+  sponge_filtered_le <- sponge_gene_miRNA_interaction_filter(gene_expr = tpm_expr,
+                                                             mir_expr = mir_expr,
+                                                             mir_predicted_targets = interactions_matrix,
+                                                             coefficient.direction = "<")
+  sponge_filtered_ge <- sponge_gene_miRNA_interaction_filter(gene_expr = tpm_expr,
+                                                             mir_expr = mir_expr,
+                                                             mir_predicted_targets = interactions_matrix,
+                                                             coefficient.direction = ">")
+}  
 
 
 
@@ -208,40 +222,11 @@ process.data.for.SPONGEinteractionFilter <- function(mir_expr, tpm_expr, interac
 #-------------------------------------------------------------------------------------
 
 
-#' Retrieve a list of TCGA samples filtered for primary disease type and sample type.
-#' NOTE: If you don not know which values to use for primary_disease and sample_type_id, use retrieve_TCGA_sample_list(sample_annotation)
-#'
-#' @param sample_annotation TCGA annotation of samples as data frame.
-#' @param primary_disease Name of the cancertype you are interested in.
-#' @param sample_type_id A number generated by TCGA as ID for sample types.
-#' @return A list of TCGA sample IDs for chosen primary disease type and sample type.
-retrieve.tcgasample.list <- function(sample_annotation, primary_disease = NULL, sample_type_id = NULL){
-
-}
-
-
-preprocess.exprdata <- function(expr_data, id_list, sample_list){
-  # Filter expression dataset for molecule IDs in id_list
-  rownames(expr_data) <- lapply(strsplit(rownames(expr_data), "\\."), '[', 1)
-  expr_data <- expr_data[rownames(expr_data) %in% id_list, ]
-  
-  # Transpose data set
-  expr_data <- as.data.frame(t(expr_data))
-  rownames(expr_data) <- gsub("\\.", "-", rownames(expr_data))
-  
-  # Filter expression dataset for sample IDs in sample_list
-  expr_data_filtered <- subset(expr_data, row.names(expr_data) %in% sample_list)
-  
-  # Spot sample IDs missing in the current expression dataset and 
-  # build matrix with values of 0 for the missing sample IDs
-  missing_sampleids <- sample_list[!(sample_list %in% row.names(expr_data))]
-  missing_expr_data <- as.data.frame(matrix(0,
-                                            length(missing_sampleids),
-                                            length(colnames(expr_data_filtered))))
-  row.names(missing_expr_data) <- missing_sampleids
-  colnames(missing_expr_data) <- colnames(expr_data_filtered)
-  
-  # Append matrix with missing sample IDs to the expression dataset
-  extended_expr_data <- rbind(expr_data_filtered, missing_expr_data)
-  return(extended_expr_data)
-}
+# #' Retrieve a list of TCGA samples filtered for primary disease type and sample type.
+# #' NOTE: If you don not know which values to use for primary_disease and sample_type_id, use retrieve_TCGA_sample_list(sample_annotation)
+# #'
+# #' @param sample_annotation TCGA annotation of samples as data frame.
+# #' @param primary_disease Name of the cancertype you are interested in.
+# #' @param sample_type_id A number generated by TCGA as ID for sample types.
+# #' @return A list of TCGA sample IDs for chosen primary disease type and sample type.
+# retrieve.tcgasample.list <- function(sample_annotation, primary_disease = NULL, sample_type_id = NULL){}
--- R/MIRRETI_breastinvasivecarcinoma.R
+++ R/MIRRETI_breastinvasivecarcinoma.R
@@ -1,20 +1,54 @@
 source("/nfs/home/students/evelyn/bachelor/R_workspace/MIRRETI/R/data_preprocess.R")
 
-#rows: mirs           cols: samples
-mir_expr <- read.expression.file("/nfs/home/students/evelyn/bachelor/data/TCGA_expression_data/pancanMiRs_EBadjOnProtocolPlatformWithoutRepsWithUnCorrectMiRs_08_04_16.xena")
-#rows: transcripts    cols: samples
-tpm_expr <- read.expression.file("/nfs/home/students/evelyn/bachelor/data/TCGA_expression_data/tcga_Kallisto_tpm")
-#$miRNA: mirs         $mRNA: transcripts      $Genesymbol: genesymbol
-utr3 <- read.interaction.file("/nfs/home/students/evelyn/bachelor/data/hsa_miRWalk_interactions/hsa_miRWalk_3UTR.txt")
-utr3$genomic_region <- rep("3UTR", nrow(utr3))
-utr5 <- read.interaction.file("/nfs/home/students/evelyn/bachelor/data/hsa_miRWalk_interactions/hsa_miRWalk_5UTR.txt")
-utr5$genomic_region <- rep("5UTR", nrow(utr5))
-cds <- read.interaction.file("/nfs/home/students/evelyn/bachelor/data/hsa_miRWalk_interactions/hsa_miRWalk_CDS.txt")
-cds$genomic_region <- rep("CDS", nrow(cds))
-#interactions <- read.interaction.file("/nfs/home/students/evelyn/bachelor/data/hsa_miRWalk_interactions/hsa_miRWalk_wholeGene.txt")
-interactions <- rbind(utr5, cds, utr3)
 
-sample_annotation <- read.sample.annotation.file("/nfs/home/students/evelyn/bachelor/data/TCGA_expression_data/cancertype_filter/TCGA_phenotype_denseDataOnlyDownload.tsv")
+#-------------------------------------------------------------------------------------
+#                                   Core data
+#-------------------------------------------------------------------------------------
 
-outfile <- "/nfs/home/students/evelyn/bachelor/data/core_data/mirreti_coredata.RDS"
-saveRDS(mir_expr, tpm_expr, interactions, sample_annotation, file = outfile)
+
+# rows: mirs           cols: samples
+# 743 obs. 10824 variables
+mir_expr <- read.expression.file("/nfs/home/students/evelyn/bachelor/data/core_data/pancanMiRs_EBadjOnProtocolPlatformWithoutRepsWithUnCorrectMiRs_08_04_16.xena")
+
+# rows: transcripts    cols: samples
+# 197044 obs. 10535 variables
+tpm_expr <- read.expression.file("/nfs/home/students/evelyn/bachelor/data/core_data/tcga_Kallisto_tpm")
+
+# $miRNA: mirs         $mRNA: transcripts      $Genesymbol: genesymbol
+# 110466891 obs. 6 variables
+interactions <- read.interaction.files("/nfs/home/students/evelyn/bachelor/data/core_data/hsa_miRWalk_5UTR.txt",
+                                       "/nfs/home/students/evelyn/bachelor/data/core_data/hsa_miRWalk_CDS.txt",
+                                       "/nfs/home/students/evelyn/bachelor/data/core_data/hsa_miRWalk_3UTR.txt")
+
+# sample    sample_type_id    sample_type   X_primary_disease
+# 12804 obs. 4 variables
+sample_annotation <- read.sample.annotation.file("/nfs/home/students/evelyn/bachelor/data/core_data/TCGA_phenotype_denseDataOnlyDownload.tsv")
+
+
+
+#-------------------------------------------------------------------------------------
+#                                   Preprocession
+#-------------------------------------------------------------------------------------
+
+
+interactions <- preproc.idconversion.refseqtoensembl(interactions)  # 116.529.799 obs. 7 variables
+
+box <- preproc.filter.samples(mir_expr, tpm_expr, sample_annotation, primary.disease = "breast invasive carcinoma", sample.type.id = "11")
+mir_expr <- box[[1]]                                # 743 obs. 90 variables
+tpm_expr <- box[[2]]                                # 197.044 obs. 113 variables
+
+mir_expr <- preproc.filter.na(mir_expr, 0)          # 485 obs. 90 variables
+tpm_expr <- preproc.filter.na(tpm_expr, -9.9658)    # 84.103 obs. 112 variables
+
+mir_expr <- preproc.filter.variance(mir_expr)       # 388 obs. 90 variables
+tpm_expr <- preproc.filter.variance(tpm_expr)       # 67.282 obs. 112 variables
+
+box <- preproc.crossfilter(mir_expr, tpm_expr, interactions)
+mir_expr <- box[[1]]                                # 378 obs. 89 variables
+tpm_expr <- box[[2]]                                # 15.834 obs. 89 variables
+interactions <- box[[3]]                            # 5.027.525 obs. 7 variables
+
+box <- preproc.data.spongefilter(mir_expr, tpm_expr, interactions)
+mir_expr <- box[[1]]                                # num [1:89, 1:378]
+tpm_expr <- box[[2]]                                # num [1:89, 1:15.834]
+interactions_matrix <- box[[3]]                     # 15.834 obs. 378 variables
